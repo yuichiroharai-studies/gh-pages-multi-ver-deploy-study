5116f0c0b41d14be9ce047e422b9a4bf33f613f7
Yuichiroh Arai <yuichiroh.arai@gmail.com>
2025-11-26 13:53:55 +0900

    ci: GitHub Pagesでバージョンごとにデプロイできるようにする

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: process.env.ASTRO_SITE || "http://localhost:4321",
+  base: process.env.ASTRO_BASE || "/",
+});
