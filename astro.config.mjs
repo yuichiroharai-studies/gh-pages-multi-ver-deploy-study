ed7e7a592422e1003d41b3e31ac5cc47b58c8cf6
Yuichiroh Arai <yuichiroh.arai@gmail.com>
2025-11-26 14:42:02 +0900

    ci: バージョン番号の取得方法を変国

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,24 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import { readFile } from "node:fs/promises";
 import tailwindcss from "@tailwindcss/vite";
+
+const pkg = JSON.parse(await readFile("./package.json", "utf-8"));
+
+const NAME = pkg.name;
+const VERSION = pkg.version;
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.ASTRO_SITE || "http://localhost:4321",
-  base: process.env.ASTRO_BASE || "/",
+  base: `/${NAME}/${VERSION}`,
   server: {
     host: true,
   },
   vite: {
+    define: {
+      "import.meta.env.VERSION": JSON.stringify(VERSION),
+    },
     plugins: [tailwindcss()],
   },
 });
